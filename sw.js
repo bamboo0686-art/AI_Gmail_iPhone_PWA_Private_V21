6795fd9e333876e34756188b0087964d43119e80
bamboo0686-art <bamboo0686@gmail.com>
2026-08-23 23:55:08 +0800

    Refresh PWA cache and checksums

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="xinyu-gmail-agent-v20-1";
+const CACHE="xinyu-gmail-agent-v20-2";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./assets/app.css","./assets/app.js","./assets/icon-192.png","./assets/icon-512.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
